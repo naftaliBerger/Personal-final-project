0de401c090d5fdc5b77a4fa18280fbbfda45d339
Naftali Berger <bn26665@gmail.com>
2025-09-07 17:59:16 +0300

    Adding tickets and most of Phase A End of day 07.09

---
+++ src/components/application-layout/Headers.tsx
@@ -0,0 +1,11 @@
+import '../../styls/Headers.css'
+export default function headers() {
+  return (
+  <div id="Header">
+    <h1>Linkodkok</h1>
+    <img id='logo' src="image.png" alt="" />
+    
+  </div>
+    
+  )
+}
